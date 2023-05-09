553f1b6984dd9e781c37267cab63d3d52da38326
nilsmanc <etereaiberia@gmail.com>
2023-05-09 07:46:01 +0300

    typeorm, files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { FilesModule } from './files/files.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [  TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'test',
+    entities: [],
+    synchronize: true,
+  }),UsersModule, FilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
