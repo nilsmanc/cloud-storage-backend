c91b798c2accb8909d3db85a7d4657f442b55027
nilsmanc <etereaiberia@gmail.com>
2023-05-20 09:38:22 +0300

    login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBody } from '@nestjs/swagger';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(AuthGuard('local'))
+  @Post('login')
+  @ApiBody({ type: CreateUserDto })
+  async login(@Request() req) {
+    return req.user;
+  }
+
+  @Post('/register')
+  register(@Body() dto: CreateUserDto) {
+    return this.authService.register(dto);
+  }
+}
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({
+    default: 'test@test.com',
+  })
   email: string;
+  @ApiProperty({
+    default: 'Name',
+  })
   fullName: string;
+  @ApiProperty({
+    default: '123',
+  })
   password: string;
 }
